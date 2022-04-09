112c3776133ecc4f08ca32591c5ef317e6e5855d
belay09 <belay1.sirak2.tsige3@gmail.com>
2022-04-09 09:53:29 -0700

    low level

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,17 @@
 * main - prints to string
 * Description: Prints "and that piece of art is useful.." without puts
 * Return: 1
-*/ 
+*/
 int main(void)
-{ char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; long l = 59; long fd = 1; 
+{
+char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; long l = 59; long fd = 1; 
 long syscall = 1;
-long ret = 0:__asm__ ("syscall"
-: "=a" (ret) "a" (syscall),
-"D" (fd), "S" (s), "d" (l));
+long ret = 0;
+__asm__ ("syscall"
+: "=a" (ret)
+: "a" (syscall),
+"D" (fd),
+"S" (s),
+"d" (l));
 return (1);
 }
