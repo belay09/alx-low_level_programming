8011b86d21aa8a7a600100367cab8e28ba0af534
belay09 <belay1.sirak2.tsige3@gmail.com>
2022-04-12 18:51:33 +0300

    programing low level

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_alphabet - check the code for ALX School students.
+*
+* Return: Always 0.
+*/
+void print_alphabet(void)
+{
+char pp = 'a';
+while (pp <= 'z')
+{
+_putchar (pp);
+pp++;
+}
+_putchar ('\n');
+}
